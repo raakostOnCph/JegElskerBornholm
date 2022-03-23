f935b8b901a9378d5d5fc50a3369b96c86e88dce
raakostOnCph <36969041+raakostOnCph@users.noreply.github.com>
2022-03-23 13:23:52 +0100

    kan vise kontoerne på forsiden

--- src/main/java/com/example/bankdemo/HelloServlet.java
+++ src/main/java/com/example/bankdemo/HelloServlet.java
@@ -1,21 +1,54 @@
 package com.example.bankdemo;
 
+import DomæneObjekter.Konto;
+
 import java.io.*;
+import java.util.Map;
+import java.util.TreeMap;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 
 @WebServlet(name = "helloServlet", value = "/hello-servlet")
 public class HelloServlet extends HttpServlet
 {
-    private String message;
+    Map<String, Konto> kontoMap = new TreeMap<>();
+
 
     public void init()
     {
+        Konto konto = new Konto("markus","1", 100);
+        Konto konto1 = new Konto("sebastian","1", 100);
+        Konto konto2 = new Konto("timmi","1", 100);
+        Konto konto3 = new Konto("march","1", 100);
+        Konto konto4 = new Konto("patrick","1", 100);
+        Konto konto5 = new Konto("otto","1", 100);
+        Konto konto6 = new Konto("maria","1", 100);
+        Konto konto7 = new Konto("nicki","1", 100);
+        Konto konto8 = new Konto("micki","1", 100);
+        Konto konto9 = new Konto("nikolaj","1", 100);
+
+        kontoMap.put(konto.getNavn(), konto);
+        kontoMap.put(konto1.getNavn(), konto1);
+        kontoMap.put(konto2.getNavn(), konto2);
+        kontoMap.put(konto3.getNavn(), konto3);
+        kontoMap.put(konto4.getNavn(), konto4);
+        kontoMap.put(konto5.getNavn(), konto5);
+        kontoMap.put(konto6.getNavn(), konto6);
+        kontoMap.put(konto7.getNavn(), konto7);
+        kontoMap.put(konto8.getNavn(), konto8);
+        kontoMap.put(konto9.getNavn(), konto9);
+
+        ServletContext servletContext = getServletContext();
+        servletContext.setAttribute("kontis",kontoMap );
 
     }
 
-    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException
     {
+         request.getRequestDispatcher("index.jsp").forward(request , response);
+
 
     }
 
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,3 +1,4 @@
+<%@ taglib prefix = "c" uri = "http://java.sun.com/jsp/jstl/core" %>
 <%@ page contentType="text/html; charset=UTF-8" pageEncoding="UTF-8" %>
 <!DOCTYPE html>
 <html>
@@ -9,5 +10,19 @@
 </h1>
 <br/>
 <a href="hello-servlet">Hello Servlet</a>
+
+<h1>liste af kontoer</h1>
+
+<c:forEach items="${applicationScope.kontis}" var="element">
+
+    ${element.value.navn} : ${element.value.saldo}
+    <br>
+
+
+
+</c:forEach>
+
+
 </body>
 </html>
+
